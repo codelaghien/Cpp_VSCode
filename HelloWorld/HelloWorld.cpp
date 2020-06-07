c067611f47b947e27246a815c1711ed164a18c0a
codelaghien <codelaghien.github.io>
2020-06-07 16:10:17 -0700

    Cai dat de viet C/C++ trong VS Code

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World ! from VS Code !";
+    cout << endl;
+    int i = 10;
+    int x = i + 2;
+
+    cout << x << endl;
+
+    char c = getch();
+}
